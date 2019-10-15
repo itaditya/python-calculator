07ba4612edd1a21b81fcfb9ec0e77351d5e7905e
Aditya Agarwal <adityaa803@gmail.com>
2019-10-16 02:02:07 +0530

    added formatter

--- Calculator.py
+++ Calculator.py
@@ -1,45 +1,43 @@
 import math
+
+
 class Calculator:
-  
-  def sum(self, nums):
-    val = 0
-    for n in nums:
-      val += n
-    result = val
-    return result
-  def multiply(self, nums):
-    val = 1
-    for n in nums:
-      val *= n
-    result = val
-    return result
-  def average(self, nums):
-    val = 0
-    l = len(nums)
-    nums_sum = self.sum(nums)
-    result = nums_sum / l
-    return result
 
+    def sum(self, nums):
+        val = 0
+        for n in nums:
+            val += n
+        result = val
+        return result
 
-  def cos(self,nums):
-    result = list(map(math.cos,nums))
+    def multiply(self, nums):
+        val = 1
+        for n in nums:
+            val *= n
+        result = val
+        return result
 
-  def sin(self,nums):
-    result = list(map(math.sin,nums))
+    def average(self, nums):
+        val = 0
+        l = len(nums)
+        nums_sum = self.sum(nums)
+        result = nums_sum / l
+        return result
 
-    return result
+    def cos(self, nums):
+        result = list(map(math.cos, nums))
+        return result
 
-  def m_to_cm(self, nums):
-  	m_to_cm_unit = 100
-  	result = list(map(lambda x:m_to_cm_unit*x, nums))
-  	return result
+    def sin(self, nums):
+        result = list(map(math.sin, nums))
 
+        return result
 
-  def tan(self,nums):
-    result = list(map(math.tan,nums))
-    return result
+    def m_to_cm(self, nums):
+        m_to_cm_unit = 100
+        result = list(map(lambda x: m_to_cm_unit * x, nums))
+        return result
 
-
-
-
-
+    def tan(self, nums):
+        result = list(map(math.tan, nums))
+        return result
--- cli.py
+++ cli.py
@@ -1,38 +1,44 @@
 import argparse
 from Calculator import Calculator
 
-method_list = [func for func in dir(Calculator) if callable(getattr(Calculator, func)) and not func.startswith("__")]
+method_list = [func for func in dir(Calculator) if callable(
+    getattr(Calculator, func)) and not func.startswith("__")]
 allowed_methods_help = '*' + '* or *'.join(method_list) + '*'
 
-parser = argparse.ArgumentParser(description='Calculate ' + allowed_methods_help + ' of the numbers you provide')
+parser = argparse.ArgumentParser(
+    description='Calculate ' + allowed_methods_help + ' of the numbers you provide')
 
-parser.add_argument('operation', type=str, help='One of the following operations: ' + allowed_methods_help)
-parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+parser.add_argument('operation', type=str,
+                    help='One of the following operations: ' + allowed_methods_help)
+parser.add_argument('integers', metavar='N', type=int,
+                    nargs='+', help='an integer for the accumulator')
 
 args = parser.parse_args()
 calc = Calculator()
 
+
 def calculate(operation, integers):
-  if (operation == 'sum'):
-    return calc.sum(integers)
-  if (operation == 'multiply'):
-    return calc.multiply(integers)
-  if (operation == 'average'):
-    return calc.average(integers)
-  
-  if (operation == 'cos'):
-    return calc.cos(integers)  
+    if (operation == 'sum'):
+        return calc.sum(integers)
+    if (operation == 'multiply'):
+        return calc.multiply(integers)
+    if (operation == 'average'):
+        return calc.average(integers)
 
-  if (operation == 'sin'):
-    return calc.sin(integers)  
+    if (operation == 'cos'):
+        return calc.cos(integers)
 
-  if (operation == 'm-cm'):
-  	return '\n'.join(map(lambda x:str(x), calc.m_to_cm(integers)))
+    if (operation == 'sin'):
+        return calc.sin(integers)
+
+    if (operation == 'm-cm'):
+        return '\n'.join(map(lambda x: str(x), calc.m_to_cm(integers)))
+
+    if (operation == 'tan'):
+        return calc.tan(integers)
+    return 0
 
 
-  if (operation == 'tan'):
-    return calc.tan(integers) 
-  return 0
 result = calculate(args.operation, args.integers)
 print('------')
 print(result)
